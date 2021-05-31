42ca6168ac97bb5e0b4eac52b08e2f5c216cf2fe
developershakil656 <71166844+developershakil656@users.noreply.github.com>
2021-05-31 00:46:58 -0500

    normal email
    normal email with view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/normal', function () {
+
+    Mail::send([],[],function($msg){
+        $msg->to('user@gmail.com','user')
+            ->subject('welcome to programming')
+            ->from('Hi@gmail.com')
+            ->setBody('thanks for register our application!');
+    });
+    echo 'mail send successful';
+});
+
+Route::get('/normal-with-view', function () {
+    $data = ['name'=>'shakil hossain', 'msg'=>'this is a normal mail with view.'];
+
+    Mail::send('mail.welcome',$data,function($msg){
+        $msg->to('user@gmail.com','user')
+            ->subject('programming');
+    });
+    echo 'mail send successful';
 });
